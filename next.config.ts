776fe9032bd844add6d373ac93aee33713423130
jingwei.song <jeevsong@gmail.com>
2025-04-07 22:19:35 +0800

    feat: 第一次提交

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
+
+
+
+(async function() {
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
+})()
+
 
 export default nextConfig;
